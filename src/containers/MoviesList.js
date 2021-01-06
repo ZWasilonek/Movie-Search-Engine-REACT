4c4e11f4808783d3fa5b8aada877a3a24cf09d6e
Zofia Wasilonek <zofiawasilonek@gmail.com>
2021-01-06 21:51:27 +0100

    MoviesList rootPath, pagination added

--- src/containers/MoviesList.js
+++ src/containers/MoviesList.js
@@ -1,10 +1,16 @@
 import { connect } from "react-redux";
 import MoviesListView from "../components/movieList/MoviesListView";
+import { setPageNumber, setRootPath } from "../redux/actions/moviesActions";
 import { isRequestLoading, getFetchingError } from "../redux/selectors/fetchingSelectors";
 
-const mapState = state => ({
+const mapState = (state) => ({
   loading: isRequestLoading(state),
   error: getFetchingError(state),
 });
 
-export default connect(mapState)(MoviesListView);
+const mapDispatch = (dispatch) => ({
+  setRootPath: (rootPath) => dispatch(setRootPath(rootPath)),
+  changePageNumber: (pageNumber) => dispatch(setPageNumber(pageNumber))
+});
+
+export default connect(mapState, mapDispatch)(MoviesListView);
