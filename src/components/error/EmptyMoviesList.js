b646f5331705696145e5ff1419bc2b209ed8bdea
Zofia Wasilonek <zofiawasilonek@gmail.com>
2021-01-06 21:45:07 +0100

    EmptyMoviesList message changed

--- src/components/error/EmptyMoviesList.js
+++ src/components/error/EmptyMoviesList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export default class EmptyMoviesList extends Component {
   render() {
-    const ERROR = "The list is empty :(";
+    const ERROR = "You haven't selected any movie yet :(";
     return (
       <div className="movies-list-error">
         <h1>{ERROR.toUpperCase()}</h1>
