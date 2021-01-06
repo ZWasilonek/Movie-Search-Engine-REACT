66f696102a5ff0ad595f4d6bc2eaacd1156f0f0c
Zofia Wasilonek <zofiawasilonek@gmail.com>
2021-01-06 21:50:45 +0100

    Home pagination, withRouted fixed

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,6 +1,10 @@
 import { connect } from "react-redux";
 import { loadMovies } from "../redux/actions/fetchingActions";
-import { getAmountOfResults, getTotalPagesNumber } from "../redux/selectors/paginationSelectors";
+import {
+  getAmountOfResults,
+  getTotalPagesNumber,
+  getHomePageIndex
+} from "../redux/selectors/paginationSelectors";
 import { getSearchTitle } from "../redux/selectors/movieDetailSelectors";
 import { getAllFetchedMovies } from "../redux/selectors/moviesSelectors";
 import HomeView from "../components/main/HomeView";
@@ -8,11 +12,13 @@ import store from "../redux/store";
 
 const mapState = state => ({
   movies: getAllFetchedMovies(state),
-  totalPages: getTotalPagesNumber(getAmountOfResults(state))
+  totalPages: getTotalPagesNumber(getAmountOfResults(state)),
+  pageIndex: getHomePageIndex(state)
 });
 
-const mapDispatch = (dispatch) => ({
-  loadMovies: page => dispatch(loadMovies(getSearchTitle(store.getState()), page))
+const mapDispatch = dispatch => ({
+  loadMovies: () => dispatch(
+    loadMovies(getSearchTitle(store.getState()), (getHomePageIndex(store.getState()) + 1))),
 });
 
 export default connect(mapState, mapDispatch)(HomeView);
