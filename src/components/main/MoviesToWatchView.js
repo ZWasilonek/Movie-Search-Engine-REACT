873f41a0572b5d5bd658a0993d432d9caa2730d7
Zofia Wasilonek <zofiawasilonek@gmail.com>
2021-01-06 21:48:02 +0100

    MoviesToWatchView pagination, withRouted fixed

--- src/components/main/MoviesToWatchView.js
+++ src/components/main/MoviesToWatchView.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom';
 import MoviesList from '../../containers/MoviesList';
 
-export default class MoviesToWatchView extends Component {
-  componentDidMount() {
-    this.props.loadSelectedPage(1);
+class MoviesToWatchView extends Component {
+  componentWillMount() {
+    this.props.loadSelectedPage();
   }
+
   render() {
     return (
       <>
         <MoviesList 
           movies={this.props.moviesToWatch}
           totalPages={this.props.totalPages}
+          pageIndex={this.props.pageIndex}
           loadSelectedPage={this.props.loadSelectedPage}
         />
       </>
     );
   };
 };
+
+export default withRouter(MoviesToWatchView);
