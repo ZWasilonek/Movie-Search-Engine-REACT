ec47036350d493bdf3e7c435eb28773cb1e6bfa1
Zofia Wasilonek <zofiawasilonek@gmail.com>
2021-01-06 21:48:59 +0100

    MovieDetailsView fixed

--- src/components/movie/MovieDetailsView.js
+++ src/components/movie/MovieDetailsView.js
@@ -2,26 +2,33 @@ import React, { Component } from "react";
 import { Helmet } from "react-helmet";
 import { Card, Container, Col, Row, Image } from "react-bootstrap";
 import Loading from "../Spinner";
-import { FetchingError } from "../error/FetchingError";
 import EmptyMoviesList from "../error/EmptyMoviesList";
 import classes from "./MovieDetails.module.css";
 import StarsRatingView from "./StarsRatingView";
+import FetchingError from "../error/FetchingError";
 
 class MovieDetailsView extends Component {
-  componentDidMount = () => {
+  componentWillMount = () => {
     this.props.loadMovieDetails();
   };
 
   render() {
     const { error, movie, loading } = this.props;
-    const backgroundStyle = "body {background: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(https://webdesignerdepot.s3.amazonaws.com/default-images/16.jpg) no-repeat fixed center / cover;}"
+    const backgroundStyle = 
+    "body {" +
+      "background: linear-gradient(" +
+          "rgba(0, 0, 0, 0.5)," + 
+          "rgba(0, 0, 0, 0.5))," +
+          "url(https://webdesignerdepot.s3.amazonaws.com/default-images/16.jpg) no-repeat fixed center / cover; " + 
+      "font-weight: 600;" +
+    "}";
     return (
       <>
         {loading && <Loading />}
         {error && <FetchingError error={error} />}
-        {movie === undefined && movie !== null && !error && !this.shouldComponentRender()
+        {movie === undefined && movie !== null && !error && !loading
           && <EmptyMoviesList />}
-        {movie !== undefined && movie !== null && !loading &&
+        {movie !== undefined && movie !== null && !loading && !error &&
           <div className={classes.MainContainer}>
             <Helmet>
               <style>{backgroundStyle}</style>
@@ -54,10 +61,8 @@ class MovieDetailsView extends Component {
                       <Card.Text><span className={classes.MovieFeatures}>
                         Actors:</span> {movie.Actors}</Card.Text>
                       <Card.Text>{movie.Plot}</Card.Text>
-                      <Card.Text>
-                        <StarsRatingView rating={movie.rating} /> 
-                        <span className={classes.MovieVotesCard}>{movie.imdbVotes} votes</span>
-                      </Card.Text>
+                      <StarsRatingView rating={movie.rating} /> 
+                      <span className={classes.MovieVotesCard}>{movie.imdbVotes} votes</span>
                     </Card.Body>
                   </Card>
                 </Col>
