f1fca2628a156cff72580cb1a1a49ea26d636bdf
Zofia Wasilonek <zofiawasilonek@gmail.com>
2021-01-14 23:19:24 +0100

    movieDetailsSelectors code readability improved

--- src/redux/selectors/movieDetailSelectors.js
+++ src/redux/selectors/movieDetailSelectors.js
@@ -1,18 +1,25 @@
 export const getMovieDetails = state => {
   const movie = state.moviesState.movieDetails;
-  if (movie !== undefined && movie !== null) validateMovie(movie);
+  if (movie !== undefined && movie !== null) {
+    validateMovie(movie);
+  }
   return movie;
 };
 
 function validateMovie(movie) {
   const NOT_AVAILABLE = "N/A";
   const { Poster } = movie;
-  if (Poster === NOT_AVAILABLE) 
-  movie.Poster = "https://www.genesisglobalschool.edu.in/wp-content/uploads/2016/09/noimage.jpg";
+  if (Poster === NOT_AVAILABLE) {
+    movie.Poster = "https://www.genesisglobalschool.edu.in/wp-content/uploads/2016/09/noimage.jpg";
+  }
   movie.rating = 0;
   Object.entries(movie).forEach(([prop, val]) => {
-    if (prop === "imdbRating" && val !== NOT_AVAILABLE) movie.rating = getFixedRating(val);
-    if (val === NOT_AVAILABLE) movie[prop] = "NOT AVAILABLE";
+    if (prop === "imdbRating" && val !== NOT_AVAILABLE) {
+      movie.rating = getFixedRating(val);
+    }
+    if (val === NOT_AVAILABLE) {
+      movie[prop] = "NOT AVAILABLE";
+    }
   });
   return movie;
 }
