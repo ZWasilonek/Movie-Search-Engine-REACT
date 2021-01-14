b592902331866bc9c68777fe5eef1ed882c480a1
Zofia Wasilonek <zofiawasilonek@gmail.com>
2021-01-14 22:10:08 +0100

    Movie changeRating added

--- src/containers/Movie.js
+++ src/containers/Movie.js
@@ -3,15 +3,17 @@ import MovieView from "../components/movie/MovieView";
 import { 
   addToWatch, 
   addToWatched, 
+  changeRating, 
   removeFromMoviesToWatch, 
   removeFromWatchedMovies,
 } from "../redux/actions/moviesActions";
 
 const mapDispatch = (dispatch) => ({
-  addToWatch: movie => dispatch(addToWatch(movie)),
+  addToWatch: (movie) => dispatch(addToWatch(movie)),
   removeFromMoviesToWatch: (movie) => dispatch(removeFromMoviesToWatch(movie)),
   addToWatched: (movie) => dispatch(addToWatched(movie)),
   removeFromWatchedMovies: (movie) => dispatch(removeFromWatchedMovies(movie)),
+  changeRating: (movie) => dispatch(changeRating(movie))
 });
 
 export default connect(null, mapDispatch)(MovieView);
