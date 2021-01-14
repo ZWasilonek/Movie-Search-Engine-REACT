53f53edbf165b54b085da6e1ec9ce0d6dc013d56
Zofia Wasilonek <zofiawasilonek@gmail.com>
2021-01-14 23:13:46 +0100

    StarsRatingView into functional component

--- src/components/movie/StarsRatingView.js
+++ src/components/movie/StarsRatingView.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react'
+import React from 'react'
 import StarRatings from 'react-star-ratings';
 
-export default class StarsRatingView extends Component {
-  render() {
-    return (
-      <StarRatings
-        rating={this.props.rating}
-        starRatedColor="rgb(255, 221, 26)"
-        starEmptyColor="rgb(250, 242, 224)"
-        numberOfStars={5}
-        name="rating"
-        starDimension="30px"
-        starSpacing="-5px"
-        changeRating={this.props.changeRating && this.props.changeRating}
-        starHoverColor="rgb(255, 193, 7)"
-      />
-    )
-  }
+export default function StarsRatingView({changeRating, rating}) {
+  return (
+    <StarRatings
+      rating={rating}
+      starRatedColor="rgb(255, 221, 26)"
+      starEmptyColor="rgb(250, 242, 224)"
+      numberOfStars={5}
+      name="rating"
+      starDimension="30px"
+      starSpacing="-5px"
+      changeRating={changeRating}
+      starHoverColor="rgb(255, 193, 7)"
+    />
+  )
 }
