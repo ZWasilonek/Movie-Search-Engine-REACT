61404117dde39c937b7accde4ab09fec5d5a377d
Zofia Wasilonek <zofiawasilonek@gmail.com>
2021-01-15 21:20:11 +0100

    fetchingActions https

--- src/redux/actions/fetchingActions.js
+++ src/redux/actions/fetchingActions.js
@@ -16,7 +16,7 @@ const loadMovies = (title, page) => async dispatch => {
 
   if (typeof page === "undefined") page = 1;
 
-  await fetch(`http://www.omdbapi.com/?s=${title}&page=${page}&type=movie&apikey=${OMDb_API_KEY}`)
+  await fetch(`https://www.omdbapi.com/?s=${title}&page=${page}&type=movie&apikey=${OMDb_API_KEY}`)
     .then(response => response.json())
     .then(data => typeof data !== "undefined" && data.Response === "False" ?
       dispatch(setError(data)) :
@@ -34,7 +34,7 @@ function setMovieTitle(title) {
 
 const loadMovieDetails = (movieID) => async dispatch => {
   dispatch(startFetching());
-  await fetch(`http://www.omdbapi.com/?i=${movieID}&plot=full&type=movie&apikey=${OMDb_API_KEY}`)
+  await fetch(`https://www.omdbapi.com/?i=${movieID}&plot=full&type=movie&apikey=${OMDb_API_KEY}`)
     .then(response => response.json())
     .then(data => typeof data !== "undefined" && data.Response === "False" ?
       dispatch(setError(data)) :
