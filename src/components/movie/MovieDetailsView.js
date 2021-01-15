5153ad192b2cf5d11e4e49e06ceffd9447804eb1
Zofia Wasilonek <zofiawasilonek@gmail.com>
2021-01-15 20:32:19 +0100

    MovieDetailsView readability improved

--- src/components/movie/MovieDetailsView.js
+++ src/components/movie/MovieDetailsView.js
@@ -14,13 +14,13 @@ export default class MovieDetailsView extends Component {
 
   render() {
     const { error, movie, loading } = this.props;
-    const backgroundStyle = 
-    "body {" +
+    const backgroundStyle =
+      "body {" +
       "background: linear-gradient(" +
-        "rgba(126, 125, 125, 0.514)," + 
-        "rgba(0, 0, 0, 0.5))," +
-        "url(https://webdesignerdepot.s3.amazonaws.com/default-images/16.jpg) no-repeat fixed center / cover; " + 
-    "}";
+      "rgba(126, 125, 125, 0.514)," +
+      "rgba(0, 0, 0, 0.5))," +
+      "url(https://webdesignerdepot.s3.amazonaws.com/default-images/16.jpg) no-repeat fixed center / cover; " +
+      "}";
     return (
       <>
         {loading && <Loading />}
@@ -37,30 +37,50 @@ export default class MovieDetailsView extends Component {
                 <Col xs={12} md={6} lg='auto'>
                   <Image src={movie.Poster} rounded />
                 </Col>
+
                 <Col xs={12} md={6} lg={7}>
                   <Card className={classes.MovieCard}>
+
                     <Card.Header className={classes.MovieTitleCard}>
                       {movie.Title.toUpperCase()} ({movie.Year})
                     </Card.Header>
+
                     <Card.Body>
-                      <Card.Text><span className={classes.MovieFeatures}>
-                        Direction:</span> {movie.Director}</Card.Text>
-                      <Card.Text><span className={classes.MovieFeatures}>
-                        Production:</span> {movie.Production}</Card.Text>
-                      <Card.Text><span className={classes.MovieFeatures}>
-                        Genre:</span> {movie.Genre}</Card.Text>
-                      <Card.Text><span className={classes.MovieFeatures}>
-                        Country:</span> {movie.Country}</Card.Text>
-                      <Card.Text><span className={classes.MovieFeatures}>
-                        Premiere:</span> {movie.Released}</Card.Text>
-                      <Card.Text><span className={classes.MovieFeatures}>
-                        Runtime:</span> {movie.Runtime}</Card.Text>
-                      <Card.Text><span className={classes.MovieFeatures}>
-                        Awards:</span> {movie.Awards}</Card.Text>
-                      <Card.Text><span className={classes.MovieFeatures}>
-                        Actors:</span> {movie.Actors}</Card.Text>
+                      <Card.Text>
+                        <span className={classes.MovieFeaturesTitle}>Direction:</span>
+                        <span>{movie.Director}</span>
+                      </Card.Text>
+                      <Card.Text>
+                        <span className={classes.MovieFeaturesTitle}>Production:</span>
+                        <span>{movie.Production}</span>
+                      </Card.Text>
+                      <Card.Text>
+                        <span className={classes.MovieFeaturesTitle}>Genre:</span>
+                        <span>{movie.Genre}</span>
+                      </Card.Text>
+                      <Card.Text>
+                        <span className={classes.MovieFeaturesTitle}>Country:</span>
+                        <span>{movie.Country}</span>
+                      </Card.Text>
+                      <Card.Text>
+                        <span className={classes.MovieFeaturesTitle}>Premiere:</span>
+                        <span>{movie.Released}</span>
+                      </Card.Text>
+                      <Card.Text>
+                        <span className={classes.MovieFeaturesTitle}>Runtime:</span>
+                        <span>{movie.Runtime}</span>
+                      </Card.Text>
+                      <Card.Text>
+                        <span className={classes.MovieFeaturesTitle}>Awards:</span>
+                        <span>{movie.Awards}</span>
+                      </Card.Text>
+                      <Card.Text>
+                        <span className={classes.MovieFeaturesTitle}>Actors:</span>
+                        <span>{movie.Actors}</span>
+                      </Card.Text>
+
                       <Card.Text>{movie.Plot}</Card.Text>
-                      <StarsRatingView rating={movie.rating} /> 
+                      <StarsRatingView rating={movie.rating} />
                       <span className={classes.MovieVotesCard}>{movie.imdbVotes} votes</span>
                     </Card.Body>
                   </Card>
